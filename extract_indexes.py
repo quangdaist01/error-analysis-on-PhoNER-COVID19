3f5d0ff70fc6b94bb2d1ac151c034a6a4b62327f
Lov <quangdaist01@gmail.com>
2021-12-18 07:52:06 +0700

    fix count_tag function

--- extract_indexes.py
+++ extract_indexes.py
@@ -18,11 +18,7 @@ def convert_string_to_list(cell):
     return ast.literal_eval(cell)
 
 
-def get_indexes_of_filtered_errors(ERROR_TYPE_DF_PATH, PICK_AN_ERROR_INDEX, PICK_A_GOLD_TAG):
-    def count_tag_in_cell(cell):
-        if PICK_A_GOLD_TAG == None:
-            len([tag for tag in cell])
-        return len([tag for tag in cell if tag == PICK_A_GOLD_TAG])
+def get_indexes_of_filtered_errors(ERROR_TYPE_DF_PATH, PICK_AN_ERROR_INDEX, count_tag_in_cell_function):
 
     df3 = pd.read_csv(ERROR_TYPE_DF_PATH)
     df3 = df3.iloc[:, :-1]
@@ -31,7 +27,7 @@ def get_indexes_of_filtered_errors(ERROR_TYPE_DF_PATH, PICK_AN_ERROR_INDEX, PICK
         df3[error] = df3[error].map(convert_string_to_list)
         df3[error] = df3[error].map(convert_cell_to_tag)
 
-    df3['Error Counts'] = df3[errors[PICK_AN_ERROR_INDEX]].map(count_tag_in_cell)
+    df3['Error Counts'] = df3[errors[PICK_AN_ERROR_INDEX]].map(count_tag_in_cell_function)
 
     indexes = []
     for i, row in df3.iterrows():
@@ -40,6 +36,6 @@ def get_indexes_of_filtered_errors(ERROR_TYPE_DF_PATH, PICK_AN_ERROR_INDEX, PICK
     return indexes
 
 ##
-# get_indexes_of_filtered_errors(r'C:\Users\quang\PycharmProjects\DL_NLP_TUH\NLP\NER\NER_Error_analysis\Output\df_error_types_PhoBERT_gold.csv',
-#                                PICK_AN_ERROR_INDEX,
-#                                PICK_A_GOLD_TAG)
+get_indexes_of_filtered_errors(r'C:\Users\quang\PycharmProjects\DL_NLP_TUH\NLP\NER\NER_Error_analysis\Output\df_error_types_PhoBERT_gold.csv',
+                               PICK_AN_ERROR_INDEX,
+                               PICK_A_GOLD_TAG)
